a1dd3b19b71b12a3f6c8d5701af27d078b785a66
Sorin Badea <62335902+sorinbadea29@users.noreply.github.com>
2020-03-22 13:34:12 +0200

    Add Local Storage

--- app.js
+++ app.js
@@ -46,6 +46,45 @@ class UI{
   };
 };
 
+class Store{
+  static getBooks() {
+    let books;
+    if(localStorage.getItem('books') === null){
+      books = [];
+    }else{
+      books = JSON.parse(localStorage.getItem('books'));
+    };
+    return books;
+  };
+
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach(function(book){
+    const ui = new UI;
+    ui.addBookToList(book);
+    });
+  };
+
+  static addBook(book){
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  };
+
+  static removeBook(isbn){
+    const books = Store.getBooks();
+    books.forEach(function(book, index){
+      if(book.isbn === isbn){
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  };
+};
+
+// DOM Loaded Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 document.querySelector('#book-form').addEventListener('submit', e => {
   const title = document.getElementById('title').value,
   author = document.getElementById('author').value,
@@ -58,6 +97,7 @@ document.querySelector('#book-form').addEventListener('submit', e => {
   }else{
     ui.showAlert('Book Added!', 'success', 1500);
     ui.addBookToList(book);
+    Store.addBook(book); 
     ui.clear();
   };
   e.preventDefault();
@@ -66,6 +106,9 @@ document.querySelector('#book-form').addEventListener('submit', e => {
 document.querySelector('#book-list').addEventListener('click', function(e){
   const ui = new UI();
   ui.deleteBook(e.target);
+  console.log(e.target.parentElement.previousElementSibling.textContent);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  
   ui.showAlert('Book Removed!', 'success', 1500);
   e.preventDefault();
 });
