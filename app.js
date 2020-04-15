5d694cf611dc92f2238520c0105e251d4817cd4b
Sorin Badea <62335902+sorinbadea29@users.noreply.github.com>
2020-04-15 13:08:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ document.querySelector('#book-form').addEventListener('submit', e => {
   
   const book = new Book(title, author, isbn);
   if(title === '' || author === '' || isbn === ''){
-    ui.showAlert('Faild!', 'error', 3000);
+    ui.setMessage('red', 'Faild!', 3000);
   }else{
-    ui.showAlert('Book Added!', 'success', 1500);
+    ui.setMessage('green', 'Book Added!', 1500);
     ui.addBookToList(book);
     Store.addBook(book); 
     ui.clear();
@@ -32,6 +32,6 @@ document.querySelector('#book-list').addEventListener('click', function(e){
   if(e.target.className === 'delete') {
     ui.deleteBook(e.target);
     Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
-    ui.showAlert('Book Removed!', 'success', 1500);
+    ui.setMessage('green', 'Book Removed!', 1500);
   };
 });
